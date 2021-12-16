f71d565a4087eff197a7367696629d6284e67851
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-15 17:28:21 -0800

    added my routes and supabase keys, and my link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import Home from './views/Home/Home';
+import BookList from './views/Books/BookList';
+import BookDetail from './views/Books/BookDetail';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
+    <div className="App">
       <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+      <BrowserRouter>
+        <header>
+          <NavLink to="/" BookList="/" exact>
+            Book List
+          </NavLink>
+          <NavLink to="/" BookDetail="/" exact>
+            Book Detail
+          </NavLink>
+        </header>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/books">
+            <BookList />
+          </Route>
+          <Route path="/books/:id" component={BookDetail}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
